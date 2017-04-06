e70eb62d35fbf441d41b1bb35d75fbe43c6f402d
x86kernel <x86kernel@archlinux.info>
2017-04-06 05:39:49 +0000

    Colorize Response for html

--- formatter/html.go
+++ formatter/html.go
@@ -1,7 +1,24 @@
 package formatter
 
+import (
+	"bytes"
+	"io"
+
+	"github.com/x86kernel/htmlcolor"
+)
+
 type htmlFormatter struct {
 	textFormatter
+}
+
+func (f *htmlFormatter) Format(writer io.Writer, data []byte) error {
+	htmlFormatter := htmlcolor.NewFormatter()
+	buf := bytes.NewBuffer(make([]byte, 0, len(data)))
+	htmlFormatter.Format(buf, data)
+
+	writer.Write(buf.Bytes())
+
+	return nil
 }
 
 func (f *htmlFormatter) Title() string {
