272fe31414e27c42fb530c9e2965888e6d41e46d
x86kernel <x86kernel@archlinux.info>
2017-04-06 06:41:46 +0000

    error handling

--- formatter/html.go
+++ formatter/html.go
@@ -14,11 +14,14 @@ type htmlFormatter struct {
 func (f *htmlFormatter) Format(writer io.Writer, data []byte) error {
 	htmlFormatter := htmlcolor.NewFormatter()
 	buf := bytes.NewBuffer(make([]byte, 0, len(data)))
-	htmlFormatter.Format(buf, data)
+	err := htmlFormatter.Format(buf, data)
 
-	writer.Write(buf.Bytes())
+	if err == io.EOF {
+		writer.Write(buf.Bytes())
+		return nil
+	}
 
-	return nil
+	return errors.New("html formatter error")
 }
 
 func (f *htmlFormatter) Title() string {
