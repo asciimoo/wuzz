624da9483c5f0ea74f34c211eff232efd116c530
Shodiq <shodiq@tokopedia.com>
2019-10-03 23:51:53 +0700

    add F12 to the documentation and help

--- config/config.go
+++ config/config.go
@@ -74,6 +74,7 @@ var DefaultKeys = map[string]map[string]string{
 		"F7":    "focus search",
 		"F8":    "focus response-headers",
 		"F9":    "focus response-body",
+		"F12":   "redirects restriction mode",
 	},
 	"url": {
 		"Enter": "submit",
