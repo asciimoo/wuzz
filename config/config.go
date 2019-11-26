107f2e8de5a41de6f59f6a4bc81eb2df91101e77
Adam Tauber <asciimoo@gmail.com>
2019-11-26 17:18:15 +0100

    [mod] rebind redirect restriction to F11 to be able to use it on linux/urxwt

--- config/config.go
+++ config/config.go
@@ -74,7 +74,7 @@ var DefaultKeys = map[string]map[string]string{
 		"F7":    "focus search",
 		"F8":    "focus response-headers",
 		"F9":    "focus response-body",
-		"F12":   "redirectRestriction",
+		"F11":   "redirectRestriction",
 	},
 	"url": {
 		"Enter": "submit",
